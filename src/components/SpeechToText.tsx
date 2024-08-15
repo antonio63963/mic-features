5a5cb9cc5e181dc27a891c5731edf0a28389dd46
antonio63963 <shotvideo25@gmail.com>
2024-08-15 20:42:01 +0300

    speech to text

---
+++ src/components/SpeechToText.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import useSpeech from "../hooks/useSpeech";
+
+const SpeechToText = () => {
+  const {
+    text,
+    isListening,
+    startListenting,
+    stopListenting,
+    hasRecognitionSupport,
+  } = useSpeech();
+  return (
+    <div>
+      {hasRecognitionSupport ? (
+        <>
+          <div>
+            <button onClick={startListenting}>Start</button>
+            {isListening ? (
+              <div>Your bowser currently listenning...</div>
+            ) : null}
+          </div>
+          {text}
+          <button onClick={stopListenting}>Stop</button>
+        </>
+      ) : (
+        <h1>Your browser doesn't support speech</h1>
+      )}
+    </div>
+  );
+};
+
+export default SpeechToText;
